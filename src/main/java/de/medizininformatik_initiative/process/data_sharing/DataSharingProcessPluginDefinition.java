b65c474d18d58cfc0a514e8ac47e56b4adb38523
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2024-07-26 11:29:28 +0200

    increase version to 1.0.1.0

--- src/main/java/de/medizininformatik_initiative/process/data_sharing/DataSharingProcessPluginDefinition.java
+++ src/main/java/de/medizininformatik_initiative/process/data_sharing/DataSharingProcessPluginDefinition.java
@@ -12,7 +12,7 @@ import dev.dsf.bpe.v1.ProcessPluginDefinition;
 
 public class DataSharingProcessPluginDefinition implements ProcessPluginDefinition
 {
-	public static final String VERSION = "1.0.0.2";
+	public static final String VERSION = "1.0.1.0";
 	public static final LocalDate RELEASE_DATE = LocalDate.of(2024, 6, 17);
 
 	@Override
