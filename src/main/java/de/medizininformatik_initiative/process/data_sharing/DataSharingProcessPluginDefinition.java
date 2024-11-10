3ed7b91467377c36fdde13a990728b413530f80e
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2024-11-10 10:19:08 +0100

    start development cycle 1.0.3.0

--- src/main/java/de/medizininformatik_initiative/process/data_sharing/DataSharingProcessPluginDefinition.java
+++ src/main/java/de/medizininformatik_initiative/process/data_sharing/DataSharingProcessPluginDefinition.java
@@ -12,7 +12,7 @@ import dev.dsf.bpe.v1.ProcessPluginDefinition;
 
 public class DataSharingProcessPluginDefinition implements ProcessPluginDefinition
 {
-	public static final String VERSION = "1.0.2.0";
+	public static final String VERSION = "1.0.3.0";
 	public static final LocalDate RELEASE_DATE = LocalDate.of(2024, 11, 10);
 
 	@Override
