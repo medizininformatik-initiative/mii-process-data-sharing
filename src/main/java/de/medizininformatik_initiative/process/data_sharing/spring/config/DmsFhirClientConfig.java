4ddce991ce042fc3c089051015ea81df75952f3c
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2025-01-15 11:40:24 +0100

    use oidc discovery to resolve token endpoint

--- src/main/java/de/medizininformatik_initiative/process/data_sharing/spring/config/DmsFhirClientConfig.java
+++ src/main/java/de/medizininformatik_initiative/process/data_sharing/spring/config/DmsFhirClientConfig.java
@@ -103,9 +103,14 @@ public class DmsFhirClientConfig
 	private String fhirStoreProxyPassword;
 
 	@ProcessDocumentation(processNames = {
-			"medizininformatik-initiativede_mergeDataSharing" }, description = "The url of the oidc provider to request access tokens (token endpoint)", example = "http://foo.baz/realms/fhir-realm/protocol/openid-connect/token")
+			"medizininformatik-initiativede_mergeDataSharing" }, description = "The base url of the oidc provider", example = "http://foo.baz/realms/fhir-realm/protocol/openid-connect/token")
 	@Value("${de.medizininformatik.initiative.data.sharing.dms.fhir.server.oauth2.issuer.url:#{null}}")
 	private String fhirStoreOAuth2IssuerUrl;
+
+	@ProcessDocumentation(processNames = {
+			"medizininformatik-initiativede_mergeDataSharing" }, description = "The path for oidc discovery protocol", recommendation = "Change default value only if path is differs from the oidc specification")
+	@Value("${de.medizininformatik.initiative.data.sharing.dms.fhir.server.oauth2.discovery.path:/.well-known/openid-configuration}")
+	private String fhirStoreOAuth2DiscoveryPath;
 
 	@ProcessDocumentation(processNames = {
 			"medizininformatik-initiativede_mergeDataSharing" }, description = "Identifier of the client (username) used for authentication when accessing the oidc provider token endpoint")
@@ -199,9 +204,9 @@ public class DmsFhirClientConfig
 					: new String(api.getProxyConfig().getPassword());
 		}
 
-		return new OAuth2TokenClient(fhirStoreOAuth2IssuerUrl, fhirStoreOAuth2ClientId, fhirStoreOAuth2ClientSecret,
-				fhirStoreOAuth2ConnectTimeout, fhirStoreOAuth2SocketTimeout, trustStoreOAuth2Path, proxyUrl,
-				proxyUsername, proxyPassword);
+		return new OAuth2TokenClient(fhirStoreOAuth2IssuerUrl, fhirStoreOAuth2DiscoveryPath, fhirStoreOAuth2ClientId,
+				fhirStoreOAuth2ClientSecret, fhirStoreOAuth2ConnectTimeout, fhirStoreOAuth2SocketTimeout,
+				trustStoreOAuth2Path, proxyUrl, proxyUsername, proxyPassword);
 	}
 
 	public DataLogger dataLogger()
