f0ad7bcc0a52ccd28b72e78f6f412748e96caa13
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2025-09-05 16:49:27 +0200

    update StructureDefinition if needed

--- src/main/java/de/medizininformatik_initiative/process/data_sharing/DataSharingProcessPluginDeploymentStateListener.java
+++ src/main/java/de/medizininformatik_initiative/process/data_sharing/DataSharingProcessPluginDeploymentStateListener.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.MetadataResource;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.StructureDefinition;
+import org.hl7.fhir.r4.model.UriType;
 import org.springframework.beans.factory.InitializingBean;
 
 import de.medizininformatik_initiative.processes.common.crypto.KeyProvider;
@@ -54,71 +59,75 @@ public class DataSharingProcessPluginDeploymentStateListener
 	@Override
 	public void onProcessesDeployed(List<String> activeProcesses)
 	{
-		// TODO: functions updateOlderCodeSystemsIfCurrentIsNewestCodeSystem
-		// added because CodeSystems with different versions cannot be used in DSF API 1.x.
-		// Remove for DSF API 2.x API where CodeSystem versioning is fixed.
+		// TODO: functions updateOlderCodeSystemsIfCurrentIsNewestCodeSystem and
+		// updateOlderStructureDefinitionIfCurrentIsNewestStructureDefinition added because CodeSystems
+		// and StructureDefinition-Extensions with different versions cannot be used in DSF API 1.x.
+		// Remove for DSF API 2.x API where CodeSystem and StructureDefinition versioning is fixed.
 
-		updateOlderCodeSystemsIfCurrentIsNewestCodeSystem(ConstantsDataSharing.CODESYSTEM_DATA_SHARING);
+		updateOlderResourcesIfCurrentIsNewestResource(ConstantsDataSharing.CODESYSTEM_DATA_SHARING, CodeSystem.class,
+				adaptCodeSystems());
 
 		if (activeProcesses.contains(ConstantsDataSharing.PROCESS_NAME_FULL_EXECUTE_DATA_SHARING))
 			dicFhirClientFactory.testConnection();
 
 		if (activeProcesses.contains(ConstantsDataSharing.PROCESS_NAME_FULL_MERGE_DATA_SHARING))
 		{
+			updateOlderResourcesIfCurrentIsNewestResource(ConstantsDataSharing.EXTENSION_URL_DIC_IDENTIFIER,
+					StructureDefinition.class, adaptStructureDefinitionExtensions());
+
 			dmsFhirClientFactory.testConnection();
 			keyProvider.createPublicKeyIfNotExists();
 		}
 	}
 
-	private void updateOlderCodeSystemsIfCurrentIsNewestCodeSystem(String url)
+	private <T extends MetadataResource> void updateOlderResourcesIfCurrentIsNewestResource(String url, Class<T> type,
+			BiConsumer<T, List<T>> converter)
 	{
-		Bundle searchResult = searchCodeSystem(url);
-		List<CodeSystem> allCodeSystems = extractCodeSystems(searchResult, url);
+		Bundle searchResult = search(type, url);
+		List<T> allResources = extractResources(searchResult, type, url);
 
-		CodeSystem currentCodeSystem = filterCurrentCodeSystem(allCodeSystems, url);
-		List<CodeSystem> olderNewerCodeSystems = filterOlderNewerCodeSystems(allCodeSystems);
+		T currentResource = filterCurrentResource(allResources, type, url);
+		List<T> olderNewerResources = filterOlderNewerResources(allResources);
 
-		if (currentIsNewestCodeSystem(olderNewerCodeSystems))
+		if (currentIsNewestResource(olderNewerResources))
 		{
-			List<CodeSystem> codeSystemsWithNonMatchingConceptCodes = filterCodeSystemsWithNonMatchingConceptCodesAndAdaptToCurrentCodeSystemConceptCodes(
-					currentCodeSystem, olderNewerCodeSystems);
-			updateCodeSystemsWithNonMatchingConceptCodes(codeSystemsWithNonMatchingConceptCodes);
+			converter.accept(currentResource, olderNewerResources);
 		}
 	}
 
-	private Bundle searchCodeSystem(String url)
+	private Bundle search(Class<? extends Resource> type, String url)
 	{
-		return api.getFhirWebserviceClientProvider().getLocalWebserviceClient().search(CodeSystem.class,
+		return api.getFhirWebserviceClientProvider().getLocalWebserviceClient().search(type,
 				Map.of("url", List.of(url)));
 	}
 
-	private List<CodeSystem> extractCodeSystems(Bundle bundle, String codeSystemUrl)
+	private <T extends MetadataResource> List<T> extractResources(Bundle bundle, Class<T> type, String url)
 	{
 		return bundle.getEntry().stream().filter(Bundle.BundleEntryComponent::hasResource)
-				.map(Bundle.BundleEntryComponent::getResource).filter(r -> r instanceof CodeSystem)
-				.map(r -> (CodeSystem) r).filter(c -> codeSystemUrl.equals(c.getUrl())).toList();
+				.map(Bundle.BundleEntryComponent::getResource).filter(type::isInstance).map(type::cast)
+				.filter(m -> url.equals(m.getUrl())).toList();
 	}
 
-	private CodeSystem filterCurrentCodeSystem(List<CodeSystem> all, String codeSystemUrl)
+	private <T extends MetadataResource> T filterCurrentResource(List<T> all, Class<T> type, String codeSystemUrl)
 	{
-		return all.stream().filter(c -> resourcesVersion.equals(c.getVersion())).findFirst()
-				.orElseThrow(() -> new RuntimeException("CodeSystem " + codeSystemUrl + "|" + resourcesVersion));
+		return all.stream().filter(m -> resourcesVersion.equals(m.getVersion())).findFirst().orElseThrow(
+				() -> new RuntimeException(type.getSimpleName() + " " + codeSystemUrl + "|" + resourcesVersion));
 	}
 
-	private List<CodeSystem> filterOlderNewerCodeSystems(List<CodeSystem> all)
+	private <T extends MetadataResource> List<T> filterOlderNewerResources(List<T> all)
 	{
 		return all.stream().filter(c -> !resourcesVersion.equals(c.getVersion())).toList();
 	}
 
-	private boolean currentIsNewestCodeSystem(List<CodeSystem> olderNewerCodeSystems)
+	private boolean currentIsNewestResource(List<? extends MetadataResource> olderNewerResources)
 	{
-		return olderNewerCodeSystems.stream().noneMatch(this::isNewerCodeSystem);
+		return olderNewerResources.stream().noneMatch(this::isNewerResource);
 	}
 
-	private boolean isNewerCodeSystem(CodeSystem codeSystem)
+	private <T extends MetadataResource> boolean isNewerResource(T resource)
 	{
 		MinorMajorVersion current = getMajorMinorVersion(resourcesVersion);
-		MinorMajorVersion olderNewer = getMajorMinorVersion(codeSystem.getVersion());
+		MinorMajorVersion olderNewer = getMajorMinorVersion(resource.getVersion());
 
 		return current.major <= olderNewer.major && current.minor < olderNewer.minor;
 	}
@@ -132,6 +141,16 @@ public class DataSharingProcessPluginDeploymentStateListener
 		}
 
 		throw new RuntimeException("Fhir resource version " + version + " does not match regex \\d\\.\\d");
+	}
+
+	private BiConsumer<CodeSystem, List<CodeSystem>> adaptCodeSystems()
+	{
+		return (currentResource, olderResources) ->
+		{
+			List<CodeSystem> codeSystemsWithNonMatchingConceptCodes = filterCodeSystemsWithNonMatchingConceptCodesAndAdaptToCurrentCodeSystemConceptCodes(
+					currentResource, olderResources);
+			updateResources(codeSystemsWithNonMatchingConceptCodes);
+		};
 	}
 
 	private List<CodeSystem> filterCodeSystemsWithNonMatchingConceptCodesAndAdaptToCurrentCodeSystemConceptCodes(
@@ -148,8 +167,34 @@ public class DataSharingProcessPluginDeploymentStateListener
 				.collect(Collectors.toSet());
 	}
 
-	private void updateCodeSystemsWithNonMatchingConceptCodes(List<CodeSystem> codeSystems)
+	private BiConsumer<StructureDefinition, List<StructureDefinition>> adaptStructureDefinitionExtensions()
 	{
-		codeSystems.forEach(c -> api.getFhirWebserviceClientProvider().getLocalWebserviceClient().update(c));
+		return (currentResource, olderResources) ->
+		{
+			List<StructureDefinition> structureDefinitionsWithNonMatchingUrls = filterStructureDefinitionsWithNonMatchingUrlAndAdaptToCurrentUrl(
+					olderResources, currentResource.getUrl());
+			updateResources(structureDefinitionsWithNonMatchingUrls);
+		};
+	}
+
+	private List<StructureDefinition> filterStructureDefinitionsWithNonMatchingUrlAndAdaptToCurrentUrl(
+			List<StructureDefinition> olderStructureDefinitions, String url)
+	{
+		List<StructureDefinition> nonMatchingUrl = olderStructureDefinitions.stream()
+				.filter(s -> s.getDifferential().getElement().stream()
+						.anyMatch(e -> "Extension.url".equals(e.getPath()) && e.getFixed() != null
+								&& e.getFixed() instanceof UriType && !url.equals(((UriType) e.getFixed()).getValue())))
+				.toList();
+
+		nonMatchingUrl.forEach(s -> s.getDifferential().getElement().stream()
+				.filter(e -> "Extension.url".equals(e.getPath())).forEach(e -> e.setFixed(new UriType(url))));
+
+		return nonMatchingUrl;
+
+	}
+
+	private void updateResources(List<? extends MetadataResource> resources)
+	{
+		resources.forEach(m -> api.getFhirWebserviceClientProvider().getLocalWebserviceClient().update(m));
 	}
 }
