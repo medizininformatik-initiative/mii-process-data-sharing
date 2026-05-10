4203a67d997c9d6130e526e82801e45b870fc0e2
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2026-05-10 21:55:09 +0200

    add comment

--- src/main/java/de/medizininformatik_initiative/process/data_sharing/service/merge/ReinsertTarget.java
+++ src/main/java/de/medizininformatik_initiative/process/data_sharing/service/merge/ReinsertTarget.java
@@ -46,6 +46,7 @@ public class ReinsertTarget implements ServiceTask
 
 		variables.setTarget(reinsertTarget);
 
+		// latestTask not updated automatically in error case
 		updateLatestTaskIfNotStartTask(api, startTask, latestTask);
 
 		variables.setBoolean(ConstantsDataSharing.BPMN_EXECUTION_VARIABLE_DATA_SHARING_MERGE_RECEIVE_ERROR_EXISTS,
